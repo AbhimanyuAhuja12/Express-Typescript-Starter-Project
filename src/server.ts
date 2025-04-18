8543c768ffe05088ba84f0adcebb4184200f69c6
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2025-04-18 20:18:26 +0530

    setup basic config for env var & server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import express from 'express';
+import { serverConfig } from './config';
 
 const app = express();
 
-const PORT: number = 3000;
 
 app.get('/ping', (req, res) => {
     res.send('Pong');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(serverConfig.PORT, () => {
+    console.log(`Server is running on http://localhost:${serverConfig.PORT}`);
     console.log(`Press Ctrl+C to stop the server`);
 });

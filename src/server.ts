008fd9312323223d9b355b2a1f5fb8f1f4011ae8
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2025-04-18 20:40:58 +0530

    added basic route and setup folder structure following MVC

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import { serverConfig } from './config';
+import v1Router from './routers/v1/index.router';
 
 const app = express();
 
 
-app.get('/ping', (req, res) => {
-    res.send('Pong');
-});
+app.use('api/v1',v1Router);
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server is running on http://localhost:${serverConfig.PORT}`);

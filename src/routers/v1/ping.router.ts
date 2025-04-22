0b36b32f77beb0c8e06d3c7f91b49c1cdb21360d
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2025-04-22 21:02:35 +0530

    done validation using zod

--- src/routers/v1/ping.router.ts
+++ src/routers/v1/ping.router.ts
@@ -1,8 +1,14 @@
-import express from "express";
-import { pingHandler } from "../../controllers/ping.controller";
+import express from 'express';
+import { pingHandler } from '../../controllers/ping.controller';
+import {  validateQueryParams } from '../../validators';
+import { pingSchema } from '../../validators/ping.validator';
 
 const pingRouter = express.Router();
 
-pingRouter.get("/", pingHandler);
+pingRouter.get('/', validateQueryParams(pingSchema), pingHandler); 
+
+pingRouter.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
 
 export default pingRouter;
